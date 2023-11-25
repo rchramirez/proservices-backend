b28a3d811621ec620bb7c277141f434e0c22a927
Richard Choque <richoqramirez@hotmail.com>
2023-11-25 19:16:29 -0300

    moved app config in src

---
+++ src/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const sequelize = require('./config/db')
+const User = require('./models/User')
+
+// Setting
+const PORT = process.env.PORT || 3000
+
+// Rutas
+app.get('/', (req, res) => {
+  User.create({
+    name: "Anton",
+    birthday: new Date(1991, 10, 26)
+  }).then(user => {
+    res.json(user)
+  })
+})
+
+// Arrancamos el servidor
+app.listen(PORT, () => {
+  console.log(`La app ha arrancado en http://localhost:${PORT}`)
+
+  // Conectarse a la base de datos
+  sequelize.authenticate().then(() => {
+    console.log("Nos hemos conectado a la base de datos")
+  }).catch(error => {
+    console.log('Se ha producido un error', error)
+  })
+})
---
+++ src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        username: "root",
+        password: "",
+        database: "proservicesdb",
+        host: "localhost"
+    }
+}
