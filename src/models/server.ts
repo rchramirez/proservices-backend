c811aa942cf8c9c66796c3658ed4eea9609bae0e
Richard Choque <richoqramirez@hotmail.com>
2023-11-26 13:32:58 -0300

    added file server.ts

---
+++ src/models/server.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from 'express';
+
+class Server {
+    private app: Application;
+    private port: string;
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || '3001';
+        this.listen();
+        this.routes();
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Application run in port ${this.port}`)
+        })
+    }
+
+    routes() {
+        this.app.get('/', (req: Request, res: Response) => {
+            res.json({
+                msg: 'API Working'
+            })
+        })
+    }
+}
+
+export default Server;
