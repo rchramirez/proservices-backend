eeaeee79d46470ce1ae57be177d93015859f4761
Richard Choque <richoqramirez@hotmail.com>
2023-11-27 01:42:46 -0300

    added file producto models

---
+++ src/models/producto.ts
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize'
+import db from '../config/db'
+
+const Producto = db.define('Producto', {
+    name: {
+        type: DataTypes.STRING
+    },
+    description: {
+        type: DataTypes.STRING
+    },
+    price: {
+        type: DataTypes.DOUBLE
+    },
+    stock: {
+        type: DataTypes.NUMBER
+    }
+}, {
+    createdAt: false,
+    updatedAt: false
+});
+
+export default Producto;
