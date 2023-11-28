5bc9b6e3cca0045434bfbda785f8d1c4254734a4
Richard Choque <richoqramirez@hotmail.com>
2023-11-28 12:51:45 -0300

    minor changes in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-export const configDB = {
-    username: "root",
-    password: "",
-    database: "proservicesdb",
-    host: "localhost"
-}
+export const PORT = process.env.PORT || 3300
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || ''
+export const DB_NAME = process.env.DB_NAME || 'proservicesdb'
+export const DB_PORT = process.env.DB_PORT || 3306
