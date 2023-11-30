10f99568cecfb3693ec37ceef8e4c43e454edf97
Richard Choque <richoqramirez@hotmail.com>
2023-11-30 02:19:48 -0300

    added validateToken to getproducts

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { deleteProduct, getProduct, getProducts, postProduct, updateProduct } from "../controllers/product";
+import validateToken from "./validate-token";
 
 const router = Router();
 
-router.get('/', getProducts);
+router.get('/', validateToken, getProducts);
 router.get('/:id', getProduct);
 router.delete('/:id', deleteProduct);
 router.post('/', postProduct);
