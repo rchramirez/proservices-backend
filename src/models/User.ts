23fe80c2780e24b89b4b60abba39c49325928e6c
Richard Choque <richoqramirez@hotmail.com>
2023-11-29 23:38:18 -0300

    added User model

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+import { DataTypes } from 'sequelize'
+import db from '../config/db'
+
+const User = db.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+export default User;
