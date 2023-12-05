b8a72cb46638b467501a7915a58722e8f5fcb672
Richard Choque <richoqramirez@hotmail.com>
2023-12-05 09:43:38 -0300

    renamed file db to database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,15 +1,20 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from 'sequelize-typescript'
 import dotenv from 'dotenv';
+import { join } from 'path';
+//import { Product } from '../models/Product';
+import { User } from '../models/User';
 
 dotenv.config();
 
 const sequelize = new Sequelize({
+    dialect: "mysql",
+    host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    dialect: "mysql",
-    host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT)
 });
+
+sequelize.addModels([User])
 
 export default sequelize;
