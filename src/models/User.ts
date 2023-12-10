5d895f7ec9e492f2648f7bf7b2918c1611b46faa
Richard Choque <richoqramirez@hotmail.com>
2023-12-10 01:10:01 -0300

    minor changes in models

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,26 +1,81 @@
-import { Table, DataType, Model, Column, PrimaryKey, AllowNull, Unique, AutoIncrement } from 'sequelize-typescript'
-import bcrypt from 'bcrypt';
+import { CreationOptional , DataTypes, InferAttributes, InferCreationAttributes, Model } from 'sequelize'
+import sequelize from '../config/database'
+import Provider from './Provider';
 
-@Table
-export class User extends Model {
-    @AutoIncrement
-    @PrimaryKey
-    @Column(DataType.INTEGER.UNSIGNED)
-    public id!: number;
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
 
-    @Unique
-    @Column(DataType.STRING)
-    public username!: string;
-
-    @Column(DataType.STRING)
-    public password!: string;
-
-    @Unique
-    @AllowNull
-    @Column(DataType.STRING)
-    public email!: string;
-
-    @Column(DataType.STRING)
-    public token!: string;
-
+    declare id: CreationOptional<number>;
+    declare firstName: string;
+    declare lastName: string;
+    declare email: string;
+    declare phone: string;
+    declare token: string;
+    declare username: string;
+    declare password: string;
+    declare documentType: string;
+    declare document: string;
+    declare isConfirmed: boolean;
+    declare photo: Buffer;
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
 }
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    firstName: {
+        type: new DataTypes.STRING(25),
+        allowNull: false
+    },
+    lastName: {
+        type: new DataTypes.STRING(25),
+        allowNull: false
+    },
+    email: {
+        type: new DataTypes.STRING(25),
+        allowNull: false,
+        unique: true
+    },
+    phone: new DataTypes.STRING(12),
+    token: new DataTypes.STRING(30),
+    username: {
+        type: new DataTypes.STRING(15),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: new DataTypes.STRING(62),
+        allowNull: false
+    },
+    documentType: {
+        type: new DataTypes.STRING(10),
+        allowNull: false
+    },
+    document: {
+        type: new DataTypes.STRING(8),
+        allowNull: false,
+        unique: true
+    },
+    isConfirmed: {
+        type: new DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    photo: DataTypes.BLOB,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+    },
+    {
+        sequelize,
+        tableName: 'users'
+});
+
+User.hasMany(Provider, {
+    sourceKey: 'id',
+    foreignKey: 'userId',
+    as: 'fkUserProviderId'
+});
+
+export default User;
