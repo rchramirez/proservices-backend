178a67b8d44a6db952b5ced2d9392eb62125a582
Richard Choque <richoqramirez@hotmail.com>
2023-12-10 01:08:08 -0300

    minor changes in database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,20 +1,15 @@
-import { Sequelize } from 'sequelize-typescript'
+import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv';
-import { join } from 'path';
-//import { Product } from '../models/Product';
-import { User } from '../models/User';
 
 dotenv.config();
 
 const sequelize = new Sequelize({
     dialect: "mysql",
-    host: process.env.DB_HOST,
     username: process.env.DB_USER,
+    host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: Number(process.env.DB_PORT)
 });
-
-sequelize.addModels([User])
 
 export default sequelize;
