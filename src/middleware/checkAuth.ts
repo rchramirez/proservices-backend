bde5600a3a4baa9d2ce0cae35fc3337fed6d873b
Richard Choque <richoqramirez@hotmail.com>
2023-12-10 01:09:18 -0300

    minor changes in middleware

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -1,17 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import User from "../models/User";
 
 dotenv.config();
 
-const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
     const headerToken = req.headers['authorization']
     console.log(headerToken);
 
     if (headerToken != undefined && headerToken.startsWith('Bearer')) {
         try {
             const bearerToken = headerToken.slice(7);
-            jwt.verify(bearerToken, process.env.JWT_SECRET_KEY || 'mysecretkey')
+            const decoded = jwt.verify(bearerToken, process.env.JWT_SECRET_KEY || 'mysecretkey') as User
+            req.body.user = await User.findByPk(decoded.id)
             next()
         } catch (error) {
             res.status(401).json({
