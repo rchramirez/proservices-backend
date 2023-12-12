d201341eac817528b0dc6a63a6980552f29b1a98
Richard Choque <richoqramirez@hotmail.com>
2023-12-12 16:07:55 -0300

    updated reference from routes in server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import express, { Application } from 'express';
-import routesProduct from './routes/provider';
 import routesUser from './routes/user';
-import User from './models/User';
+import routesAdministrator from './routes/administrator';
+import routesProvider from './routes/provider';
+import routesConsumer from './routes/consumer';
+import routesService from './routes/service';
+import routesPublication from './routes/publication';
+import routesTask from './routes/task';
+import routesWork from './routes/work';
 
 import bodyParser from 'body-parser';
 import uuidMiddleware from './middleware/uuidMiddleware';
-import Service from './models/Service';
+
+import User from './models/User';
 import Administrator from './models/Administrator';
 import Provider from './models/Provider';
 import Consumer from './models/Consumer';
+import Service from './models/Service';
 import Publication from './models/Publication';
 import Task from './models/Task';
 import Work from './models/Work';
@@ -39,8 +46,14 @@ class Server {
     }
 
     routes() {        
-        this.app.use('/api/providers', routesProduct);
         this.app.use('/api/users', routesUser);
+        this.app.use('/api/administrator', routesAdministrator);
+        this.app.use('/api/providers', routesProvider);
+        this.app.use('/api/consumers', routesConsumer);
+        this.app.use('/api/services', routesService);
+        this.app.use('/api/publications', routesPublication);
+        this.app.use('/api/tasks', routesTask);
+        this.app.use('/api/works', routesWork);
     }
 
     async dbConnect() {
